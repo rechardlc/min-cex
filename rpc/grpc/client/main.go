f5317edf300aa242ed5f2a5978c283a4cf05cf25
chaolc <523854439@qq.com>
2026-05-18 14:45:03 +0800

    feat:grpc  proto demo

--- rpc/grpc/client/main.go
+++ rpc/grpc/client/main.go
@@ -18,18 +18,25 @@ func main() {
 	if  err != nil {
 		log.Fatal("链接失败")
 	}
+	// defer: 延迟关闭连接
 	defer conn.Close()
+	// 创建一个grpc客户端
 	client := pb_user.NewUserServiceClient(conn)
-	ctx, cancle := context.WithTimeout(context.Background(), time.Second * 1)
+	// 创建一个上下文
+	ctx, cancle := context.WithTimeout(context.Background(), time.Second * 100)
 	defer cancle()
+	// 创建一个请求
 	result, err := client.GetUser(ctx, &pb_user.GetUserRequest{
 		UserId: "a87",
+		A: 1,
+		B: 2,
 	})
-	time.Sleep(2 * time.Second)
 	if err != nil {
 		log.Fatal("请求失败")
 	}
+	// 打印结果
 	fmt.Println(result)
+	// 打印时间: time.Since(start) 返回的是一个时间差
 	elapsed := time.Since(start)
-	fmt.Println(elapsed.Seconds())
+	fmt.Println(elapsed.Seconds()) // 打印时间差
 }
--- rpc/grpc/server/main.go
+++ rpc/grpc/server/main.go
@@ -9,7 +9,7 @@ import (
 	"net"
 	"strings"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc" // 引入grpc包
 )
 
 type UserService struct {
@@ -20,6 +20,7 @@ type UserService struct {
 func (s *UserService) GetUser(ctx context.Context, req *pb_user.GetUserRequest) (*pb_user.GetUserResponse, error) {
 	fmt.Println("服务进来了")
 	if id := req.UserId; id != "" {
+		// 使用strings.Builder来拼接字符串
 		var builder strings.Builder
 		builder.WriteString("user_")
 		builder.WriteString(id)
@@ -29,19 +30,26 @@ func (s *UserService) GetUser(ctx context.Context, req *pb_user.GetUserRequest) 
 		return &pb_user.GetUserResponse{
 			UserId:   id,
 			Nickname: result,
+			Sum: sum(req.A, req.B),
 		}, nil
 	}
 	return nil, errors.New("user id is required")	
 }
 
 func main() {
+	// 监听TCP端口
 	listen, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("监听失败:", err)
 	}
+	// 创建一个grpc服务器
 	server := grpc.NewServer()
 	// new(UserService) 与 &UserService{}是一个意思，golang社区更加偏向第二种写法
 	pb_user.RegisterUserServiceServer(server, &UserService{})
 	log.Println("服务启动")
 	server.Serve(listen)
+}
+
+func sum(a float64, b float64) float64{
+	return a + b
 }
